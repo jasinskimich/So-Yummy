95c810a8d5fda0672733433f62362b4b2ef90cf8
Michał Jasiński <jasinskimichal.1996@gmail.com>
2023-09-21 14:23:33 +0200

    changes

--- packages/frontend/src/components/HomeCategories/HomeCategories.jsx
+++ packages/frontend/src/components/HomeCategories/HomeCategories.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import styles from "./HomeCategories.module.css";
 import cat1 from "../../images/cat1.png";
-import cat2 from "../../images/cat2.png";
-import cat3 from "../../images/cat3.png"
-import cat4 from "../../images/cat4.png";
+// import cat2 from "../../images/cat2.png";
+// import cat3 from "../../images/cat3.png"
+// import cat4 from "../../images/cat4.png";
 
 
 const HomeCategories = () => {
@@ -22,36 +22,6 @@ const HomeCategories = () => {
             />
             <div className={styles.categoryItemBox}>
               <span className={styles.categoryItemText}>Banan Pancakes</span>
-            </div>
-          </div>
-          <div className={styles.categoryItem}>
-            <img
-              src={cat2}
-              alt="Ham hock colcannon"
-              className={styles.categoryItemPic}
-            />
-            <div className={styles.categoryItemBox}>
-              <span className={styles.categoryItemText}>Ham hock colcannon</span>
-            </div>
-          </div>
-          <div className={styles.categoryItem}>
-            <img
-              src={cat3}
-              alt="Polish Pancakes"
-              className={styles.categoryItemPic}
-            />
-            <div className={styles.categoryItemBox}>
-              <span className={styles.categoryItemText}>Polish Pancakes</span>
-            </div>
-          </div>
-          <div className={styles.categoryItem}>
-            <img
-              src={cat4}
-              alt="Boxty Breakfast"
-              className={styles.categoryItemPic}
-            />
-            <div className={styles.categoryItemBox}>
-              <span className={styles.categoryItemText}>Boxty Breakfast</span>
             </div>
           </div>
         </div>
