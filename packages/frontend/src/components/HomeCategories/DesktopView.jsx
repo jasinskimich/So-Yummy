cfbdc158478b7630a16ef924c9b575a731fce81d
Michał Jasiński <jasinskimichal.1996@gmail.com>
2023-09-22 20:17:51 +0200

    chnages

--- packages/frontend/src/components/HomeCategories/DesktopView.jsx
+++ packages/frontend/src/components/HomeCategories/DesktopView.jsx
@@ -3,21 +3,21 @@ import cat1 from "../../images/cat1.png";
 import cat2 from "../../images/cat2.png";
 import cat3 from "../../images/cat3.png";
 import cat4 from "../../images/cat4.png";
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 const DesktopView = () => {
-
   const [categories, setCategories] = useState([]);
-  console.log(categories, "categoriesSTATE")
+  const [first, setFirst] = useState([]);
+  const [second, setSecond] = useState([]);
+  const [third, setThird] = useState([]);
+  const [fourth, setFourth] = useState([]);
 
   useEffect(() => {
-  
     const fetchCategories = async () => {
       const url = "https://yummly2.p.rapidapi.com/categories/list";
       const options = {
         method: "GET",
         headers: {
-          "X-RapidAPI-Key":
-          process.env.REACT_APP_RAPID_API_KEY,
+          "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
           "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
         },
       };
@@ -29,7 +29,6 @@ const DesktopView = () => {
         const cusines = browseCategories[8];
         const categories = cusines.display.categoryTopics;
         setCategories(categories);
-        console.log(categories, "categories");
       } catch (error) {
         console.error(error);
       }
@@ -38,58 +37,155 @@ const DesktopView = () => {
     fetchCategories();
   }, []);
 
+  useEffect(() => {
+    const fetchDetails = async () => {
+      const url = 'https://yummly2.p.rapidapi.com/feeds/list?limit=24&start=0&tag=list.recipe.search_based%3Afq%3Aattribute_s_mv%3A(cuisine%5C%5Ecuisine%5C-american)';
+      const options = {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+          "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
+        },
+      };
+
+      try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        const categoryItems =  result.feed;
+        setFirst(categoryItems)
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchDetails();
+  }, []);
+
+  useEffect(() => {
+    const fetchDetails = async () => {
+      const url = 'https://yummly2.p.rapidapi.com/feeds/list?limit=24&start=0&tag=list.recipe.search_based%3Afq%3Aattribute_s_mv%3A(cuisine%5C%5Ecuisine%5C-barbecue%5C-bbq)';
+      const options = {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+          "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
+        },
+      };
+
+      try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        const categoryItems =  result.feed;
+        setSecond(categoryItems)
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchDetails();
+  }, []);
+
+  useEffect(() => {
+    const fetchDetails = async () => {
+      const url = 'https://yummly2.p.rapidapi.com/feeds/list?limit=24&start=0&tag=list.recipe.search_based%3Afq%3Aattribute_s_mv%3A(cuisine%5C%5Ecuisine%5C-asian)';
+      const options = {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+          "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
+        },
+      };
+
+      try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        const categoryItems =  result.feed;
+        setThird(categoryItems)
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchDetails();
+  }, []);
+  useEffect(() => {
+    const fetchDetails = async () => {
+      const url = 'https://yummly2.p.rapidapi.com/feeds/list?limit=24&start=0&tag=list.recipe.search_based%3Afq%3Aattribute_s_mv%3A(cuisine%5C%5Ecuisine%5C-italian)';
+      const options = {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+          "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
+        },
+      };
+
+      try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        const categoryItems =  result.feed;
+        setFourth(categoryItems)
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchDetails();
+  }, []);
+
   return (
     <>
       <div className={styles.categoryContainer}>
         <div className={styles.categoryHead}>
-          <span className={styles.categoryHeadText}>{categories.length > 0 && categories[0].display.displayName}</span>
+          <span className={styles.categoryHeadText}>
+            {categories.length > 0 && categories[0].display.displayName}
+          </span>
         </div>
         <div className={styles.categoryContent}>
           <div className={styles.categoryItem}>
             <img
-              src={cat1}
+              src={first.length > 0 && first[0].display.images[0]}
               alt="nalesniki"
               className={styles.categoryItemPic}
             />
             <div className={styles.categoryItemBox}>
               <span className={styles.categoryItemText}>
-              Melt-In-Your-Mouth Baked Chicken Breasts
+              {first.length > 0 && first[0].display.displayName}
               </span>
             </div>
           </div>
           <div className={styles.categoryItem}>
             <img
-              src={cat2}
+              src={first.length > 0 && first[1].display.images[0]}
               alt="nalesniki"
               className={styles.categoryItemPic}
             />
             <div className={styles.categoryItemBox}>
               <span className={styles.categoryItemText}>
-                Portuguese prego Por
+              {first.length > 0 && first[1].display.displayName}
               </span>
             </div>
           </div>
           <div className={styles.categoryItem}>
             <img
-              src={cat3}
+              src={first.length > 0 && first[2].display.images[0]}
               alt="nalesniki"
               className={styles.categoryItemPic}
             />
             <div className={styles.categoryItemBox}>
               <span className={styles.categoryItemText}>
-                Portuguese prego Por
+              {first.length > 0 && first[2].display.displayName}
               </span>
             </div>
           </div>
           <div className={styles.categoryItem}>
             <img
-              src={cat4}
+              src={first.length > 0 && first[3].display.images[0]}
               alt="nalesniki"
               className={styles.categoryItemPic}
             />
             <div className={styles.categoryItemBox}>
               <span className={styles.categoryItemText}>
-                Portuguese prego Por
+              {first.length > 0 && first[3].display.displayName}
               </span>
             </div>
           </div>
@@ -100,54 +196,56 @@ const DesktopView = () => {
       </div>
       <div className={styles.categoryContainer}>
         <div className={styles.categoryHead}>
-          <span className={styles.categoryHeadText}>{categories.length > 0 && categories[1].display.displayName}</span>
+          <span className={styles.categoryHeadText}>
+            {categories.length > 0 && categories[1].display.displayName}
+          </span>
         </div>
         <div className={styles.categoryContent}>
           <div className={styles.categoryItem}>
             <img
-              src={cat1}
+              src={second.length > 0 && second[0].display.images[0]}
               alt="nalesniki"
               className={styles.categoryItemPic}
             />
             <div className={styles.categoryItemBox}>
               <span className={styles.categoryItemText}>
-                Portuguese prego Por
+              {second.length > 0 && second[0].display.displayName}
               </span>
             </div>
           </div>
           <div className={styles.categoryItem}>
             <img
-              src={cat2}
+              src={second.length > 0 && second[1].display.images[0]}
               alt="nalesniki"
               className={styles.categoryItemPic}
             />
             <div className={styles.categoryItemBox}>
               <span className={styles.categoryItemText}>
-                Portuguese prego Por
+              {second.length > 0 && second[1].display.displayName}
               </span>
             </div>
           </div>
           <div className={styles.categoryItem}>
             <img
-              src={cat3}
+              src={second.length > 0 && second[2].display.images[0]}
               alt="nalesniki"
               className={styles.categoryItemPic}
             />
             <div className={styles.categoryItemBox}>
               <span className={styles.categoryItemText}>
-                Portuguese prego Por
+              {second.length > 0 && second[2].display.displayName}
               </span>
             </div>
           </div>
           <div className={styles.categoryItem}>
             <img
-              src={cat4}
+              src={second.length > 0 && second[3].display.images[0]}
               alt="nalesniki"
               className={styles.categoryItemPic}
             />
             <div className={styles.categoryItemBox}>
               <span className={styles.categoryItemText}>
-                Portuguese prego Por
+              {second.length > 0 && second[3].display.displayName}
               </span>
             </div>
           </div>
@@ -158,54 +256,56 @@ const DesktopView = () => {
       </div>
       <div className={styles.categoryContainer}>
         <div className={styles.categoryHead}>
-          <span className={styles.categoryHeadText}>{categories.length > 0 && categories[2].display.displayName}</span>
+          <span className={styles.categoryHeadText}>
+            {categories.length > 0 && categories[2].display.displayName}
+          </span>
         </div>
         <div className={styles.categoryContent}>
           <div className={styles.categoryItem}>
             <img
-              src={cat1}
+              src={third.length > 0 && third[0].display.images[0]}
               alt="nalesniki"
               className={styles.categoryItemPic}
             />
             <div className={styles.categoryItemBox}>
               <span className={styles.categoryItemText}>
-                Portuguese prego Por
+              {third.length > 0 && third[0].display.displayName}
               </span>
             </div>
           </div>
           <div className={styles.categoryItem}>
             <img
-              src={cat2}
+              src={third.length > 0 && third[1].display.images[0]}
               alt="nalesniki"
               className={styles.categoryItemPic}
             />
             <div className={styles.categoryItemBox}>
               <span className={styles.categoryItemText}>
-                Portuguese prego Por
+              {third.length > 0 && third[1].display.displayName}
               </span>
             </div>
           </div>
           <div className={styles.categoryItem}>
             <img
-              src={cat3}
+              src={third.length > 0 && third[2].display.images[0]}
               alt="nalesniki"
               className={styles.categoryItemPic}
             />
             <div className={styles.categoryItemBox}>
               <span className={styles.categoryItemText}>
-                Portuguese prego Por
+              {third.length > 0 && third[2].display.displayName}
               </span>
             </div>
           </div>
           <div className={styles.categoryItem}>
             <img
-              src={cat4}
+              src={third.length > 0 && third[3].display.images[0]}
               alt="nalesniki"
               className={styles.categoryItemPic}
             />
             <div className={styles.categoryItemBox}>
               <span className={styles.categoryItemText}>
-                Portuguese prego Por
+              {third.length > 0 && third[3].display.displayName}
               </span>
             </div>
           </div>
@@ -216,54 +316,56 @@ const DesktopView = () => {
       </div>
       <div className={styles.categoryContainer}>
         <div className={styles.categoryHead}>
-          <span className={styles.categoryHeadText}>{categories.length > 0 && categories[3].display.displayName}</span>
+          <span className={styles.categoryHeadText}>
+            {categories.length > 0 && categories[3].display.displayName}
+          </span>
         </div>
         <div className={styles.categoryContent}>
           <div className={styles.categoryItem}>
             <img
-              src={cat1}
+              src={fourth.length > 0 && fourth[0].display.images[0]}
               alt="nalesniki"
               className={styles.categoryItemPic}
             />
             <div className={styles.categoryItemBox}>
               <span className={styles.categoryItemText}>
-                Portuguese prego Por
+              {fourth.length > 0 && fourth[0].display.displayName}
               </span>
             </div>
           </div>
           <div className={styles.categoryItem}>
             <img
-              src={cat2}
+              src={fourth.length > 0 && fourth[1].display.images[0]}
               alt="nalesniki"
               className={styles.categoryItemPic}
             />
             <div className={styles.categoryItemBox}>
               <span className={styles.categoryItemText}>
-                Portuguese prego Por
+              {fourth.length > 0 && fourth[0].display.displayName}
               </span>
             </div>
           </div>
           <div className={styles.categoryItem}>
             <img
-              src={cat3}
+              src={fourth.length > 0 && fourth[2].display.images[0]}
               alt="nalesniki"
               className={styles.categoryItemPic}
             />
             <div className={styles.categoryItemBox}>
               <span className={styles.categoryItemText}>
-                Portuguese prego Por
+              {fourth.length > 0 && fourth[0].display.displayName}r
               </span>
             </div>
           </div>
           <div className={styles.categoryItem}>
             <img
-              src={cat4}
+              src={fourth.length > 0 && fourth[3].display.images[0]}
               alt="nalesniki"
               className={styles.categoryItemPic}
             />
             <div className={styles.categoryItemBox}>
               <span className={styles.categoryItemText}>
-                Portuguese prego Por
+              {fourth.length > 0 && fourth[0].display.displayName}
               </span>
             </div>
           </div>
